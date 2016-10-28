d8e9a9a3bcdda9462262f394bf0ccbb1d9ba195f
dhowe <github.utensil@mailhero.io>
2016-10-29 05:14:57 +0800

    working version, minus pagination, @cqx931

--- fetch-students.sh
+++ fetch-students.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
-file=repos.js
+# NOTE: needs to add pagination support: https://developer.github.com/guides/getting-started/#pagination
+
+assignment="imc-assignment-9"
+
+#
+# finds all private repos for $assignment not including 'test' in the name
+#
+# usage: $ ./fetch-students.sh [username]
+#
+
+in=repos.js
+out=students.txt
 usr=${1:-$(whoami)}
-curl  -u ${usr}  https://api.github.com/orgs/SchoolOfCreativeMedia/repos?type=private > ${file}
-cat ${file} | jq '.[] | .name'
+
+curl -u ${usr}  https://api.github.com/orgs/SchoolOfCreativeMedia/repos?type=private > ${in}
+cat ${in} | jq '.[] | .name' | egrep  ${assignment} | grep -v test | sed 's/"//g' #> ${out}
